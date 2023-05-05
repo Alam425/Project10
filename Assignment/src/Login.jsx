2ada66ba81f500e66669650d0687ce84b0d1224a
Alam425 <body.flexer.627492@gmail.com>
2023-05-05 11:52:02 +0600

    fifth

--- Assignment/src/Login.jsx
+++ Assignment/src/Login.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Navbar from './Navbar';
 import { Link, useNavigate } from 'react-router-dom';
 import Footer from './footer';
+import app from './firebase.config';
 
 const Login = () => {
     const navigate = useNavigate();
     const goBack = () => {
         navigate(-1);
+    }
+    const auth = getAuth(app);
+    const provider = Goo
+
+    const googleSign = () => {
+
     }
     return (
         <div>
@@ -14,7 +21,7 @@ const Login = () => {
             <div className='ms-10 p-5'>
                 <p className="text-xl mb-5">Login with...</p>
                 <button className="p-5 me-5 btn btn-primary"><Link to='/emailLogin'>Email</Link></button>
-                <button className="p-5 me-5 btn btn-secondary">Google</button>
+                <button onClick={() => googleSign()} className="p-5 me-5 btn btn-secondary">Google</button>
                 <button className="p-5 me-5 btn btn-accent">Github</button>
             </div>
             <div className="mx-auto">
