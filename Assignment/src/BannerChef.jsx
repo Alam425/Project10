fe2944e61549509f48f58c9e4d4521be87c22e26
Alam425 <body.flexer.627492@gmail.com>
2023-07-21 17:12:02 +0600

    haha

--- Assignment/src/BannerChef.jsx
+++ Assignment/src/BannerChef.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BannerChef = ({ chef }) => {
-    const { name, years_of_experience, likes, number_of_recipes, genre } = chef;
+    const { name, years_of_experience, likes, number_of_recipes, genre, id } = chef;
     return (
         <div className='w-96 mx-auto m-5'>
                 <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -11,6 +12,7 @@ const BannerChef = ({ chef }) => {
                         <p>Years Of Experience: {years_of_experience}</p>
                         <p>Total Recipes: {number_of_recipes}</p>
                         <p>Likes: {likes}</p>
+                        <Link to={`/recipes/${genre}/${id}`}><button className="btn w-full mx-auto btn-warning">View Recipes</button></Link>
                     </div>
                 </div> 
         </div>
